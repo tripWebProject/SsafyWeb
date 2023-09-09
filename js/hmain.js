d3b7a2cd1f9e4adbdbcd19325c86ebe92a4f7389
강대은 <eodms4334@naver.com>
2023-09-09 21:58:09 +0900

    음악플레이어 버튼 및 이벤트 추가

--- js/hmain.js
+++ js/hmain.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("btn");
 const nav = document.getElementById("nav");
-btn.addEventListener("click", function () { 
+btn.addEventListener("click", function () {
     console.log("클릭");
     btn.classList.toggle("active"); //스르륵 움직이기
     nav.classList.toggle("active"); //스르륵 움직이기
@@ -10,3 +10,18 @@ btn.addEventListener("click", function () {
     }
 });
 
+function musicPurpleBtn() {
+
+    // 토글 할 버튼 선택 (btn1)
+    const mPlayer = document.getElementById('musicPlayer');
+
+    // btn1 숨기기 (display: none)
+    if (mPlayer.style.display !== 'none') {
+        mPlayer.style.display = 'none';
+    }
+    // btn` 보이기 (display: block)
+    else {
+        mPlayer.style.display = 'block';
+    }
+
+}
